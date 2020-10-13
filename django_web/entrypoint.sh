db06183379db61ef2f426d4f5b7869f04a07c520
Mr. Parak <zzz6866@gmail.com>
2020-10-13 14:17:46 +0900

    django wine Dockerfile 작성

--- django_web/entrypoint.sh
+++ django_web/entrypoint.sh
@@ -4,3 +4,10 @@ if test -e /alldev/requirements.txt; then \
     pip install -r /alldev/requirements.txt && echo "pip install success !!"; \
 fi
 
+# Apply database migrations
+echo "Apply database migrations"
+make migrate
+
+# Start server
+echo "Start server"
+python school/manage.py runserver 0.0.0.0:8000
